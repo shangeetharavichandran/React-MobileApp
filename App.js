016971affe97cd9888088d3eacfd4d33740ba868
shangeetha07 <shangeetharavichandran@gmail.com>
2018-07-24 15:06:03 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+} from 'react-native';
+
+import App from './Application';
+
+export default class Pagejump extends Component {
+    render() {
+        return (
+            <App/>
+        );
+    }
+}
+
+AppRegistry.registerComponent('Pagejump', () => Pagejump);
